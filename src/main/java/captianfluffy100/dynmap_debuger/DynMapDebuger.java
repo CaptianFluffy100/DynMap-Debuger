2369fbc5502683eb1454720111de3014f8ecffe6
DemonChildINC <jarom.brenkman@brenkmanandcompany.com>
2023-05-19 20:25:30 -0600

    Loaded chunks are now displayed

--- src/main/java/captianfluffy100/dynmap_debuger/DynMapDebuger.java
+++ src/main/java/captianfluffy100/dynmap_debuger/DynMapDebuger.java
@@ -9,6 +9,7 @@ import org.dynmap.markers.MarkerAPI;
 import org.dynmap.markers.MarkerSet;
 import org.dynmap.markers.AreaMarker;
 import org.dynmap.markers.Marker;
+import org.dynmap.markers.CircleMarker;
 
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -24,8 +25,11 @@ public class DynMapDebuger implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("dynmapdebuger");
-	private MarkerAPI markerAPI;
+	MarkerAPI markerAPI;
     private String logHeader = "[DynMap Debuger]: ";
+    private DynmapCommonAPI dynampAPI;
+    Integer count = 0;
+    MarkerSet set;
 
 	@Override
 	public void onInitialize() {
@@ -38,12 +42,23 @@ public class DynMapDebuger implements ModInitializer {
     public void onEnable() {
         DynmapCommonAPIListener.register(new DynmapCommonAPIListener() {
             public void apiEnabled(DynmapCommonAPI api) {
+                dynampAPI = api;
                 // Get the MarkerAPI instance
                 markerAPI = api.getMarkerAPI();
+                set = markerAPI.getMarkerSet("Loaded Chunks");
+                if (set == null) {
+                    set = markerAPI.createMarkerSet("Loaded Chunks", "Loaded Chunks", null, true);
+                }
                 LOGGER.info(logHeader + "(API) " + markerAPI);
+                String version = api.getDynmapCoreVersion();
+                LOGGER.info(logHeader + "(API) Version: " + version);
             }
             public void apiDisabled(DynmapCommonAPI api) {
                 LOGGER.info(logHeader + "(API) Dynmap closed or crashed");
+            }
+            public void apiListenerAdded() {
+                LOGGER.info(logHeader + "API listener added");
+                // Perform additional initialization or configuration tasks here
             }
         });
         ServerChunkEvents.CHUNK_LOAD.register((world, chunk) -> {
@@ -57,6 +72,10 @@ public class DynMapDebuger implements ModInitializer {
 
     // Listener for chunk load/unload events
     public void onChunkLoad(ServerWorld world, WorldChunk chunk) {
+        if (!dynampAPI.markerAPIInitialized()) {
+            LOGGER.info(logHeader + "Marker API not Initialized");
+            return;
+        }
         if (markerAPI == null) {
             // initDynmap();
             LOGGER.info(logHeader + "Failed");
@@ -69,45 +88,33 @@ public class DynMapDebuger implements ModInitializer {
     
         // Create a marker ID based on the dimension and chunk coordinates
         String markerId = dimension + "_" + chunkPos.x + "_" + chunkPos.z;
-        // LOGGER.info(logHeader + "CHUNK LOADED: " + markerId);
-        
-        // Check if the marker already exists
-        MarkerSet set = markerAPI.getMarkerSet(markerId);
-        if (set == null) {
-            // Create a new MarkerSet
-            set = markerAPI.createMarkerSet(markerId, "Chunk " + chunkPos.x + ", " + chunkPos.z, null, false);
-        } else {
-            // Update existing marker (in case of chunk reload)
-            set.setMarkerSetLabel("Chunk " + chunkPos.x + ", " + chunkPos.z);
-            set.setHideByDefault(false);
-            set.setLayerPriority(0);
-            set.setMinZoom(0);
-            set.setMaxZoom(10);
-        }
-
+        // LOGGER.info(logHeader + "CHUNK LOADED: " + markerId + " || " + count);
         // Check if the marker already exists in the marker set
-        AreaMarker existingMarker = set.findAreaMarker(markerId);
+        CircleMarker existingMarker = set.findCircleMarker(markerId);
         if (existingMarker != null) {
             // Marker already exists, no need to create a new one
             LOGGER.info(logHeader + "AREA MARKER ALL READY CREATED: " + markerId);
+            existingMarker.setFillStyle(0.5, 0x00FF00); // Green color with 50% transparency
+            existingMarker.setLineStyle(1, 1.0, 0x00FF00);
             return;
         }
-    
+        ++count;
         // Create a green square area marker for the chunk
-        double[] coordinates_1 = {(chunkPos.x + 1) * 16, 0, (chunkPos.z + 1) * 16};
-        double[] coordinates_2 = {chunkPos.x * 16, 0, chunkPos.z * 16};
-        AreaMarker marker = set.createAreaMarker(
+        CircleMarker marker = set.createCircleMarker(
             markerId,
             "Chunk " + chunkPos.x + ", " + chunkPos.z,
-            false,
-            world.getRegistryKey().getValue().toString(),
-            coordinates_1,
-            coordinates_2,
+            true,
+            "world",
+            (chunkPos.x * 16) + 8,
+            0,
+            (chunkPos.z * 16) + 8,
+            8,
+            8,
             false
         );
+        // LOGGER.info(logHeader + "MARKER: " + world.getRegistryKey().getValue().toString());
         marker.setFillStyle(0.5, 0x00FF00); // Green color with 50% transparency
-        // marker.setPersistent(false); // Set the marker as non-persistent
-        // Marker marker = set.createMarker(markerId, "<div>"+markerId+"</div>", true, "world", chunkPos.x * 16, (chunkPos.x + 1) * 16, chunkPos.z * 16, null, false);
+        marker.setLineStyle(1, 1.0, 0x00FF00);
     }
 
     public void onChunkUnload(ServerWorld world, WorldChunk chunk) {
@@ -123,12 +130,15 @@ public class DynMapDebuger implements ModInitializer {
     
         // Create a marker ID based on the dimension and chunk coordinates
         String markerId = dimension + "_" + chunkPos.x + "_" + chunkPos.z;
-        // LOGGER.info("CHUNK UNLOADED: " + markerId);
+        // LOGGER.info(logHeader + "CHUNK UNLOADED: " + markerId + " || " + count);
     
         // Check if the marker exists and remove it
-        MarkerSet markerSet = markerAPI.getMarkerSet(markerId);
-        if (markerSet != null) {
-            markerSet.deleteMarkerSet();
+        CircleMarker marker = set.findCircleMarker(markerId);
+        if (marker != null) {
+            // marker.setLineStyle(1, 0.0, 0x00FF00);
+            // marker.setFillStyle(0.0, 0x00FF00);
+            marker.deleteMarker();
         }
+        --count;
     }
 }
